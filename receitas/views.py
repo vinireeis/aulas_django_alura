2c2405d6ddaa8814da72c85c77b6ecbe3bbb3469
Vinicius dos Reis Oliveira <v_reis@outlook.com.br>
2021-10-20 18:53:14 -0300

    utilizando partials e embedando código py

--- receitas/views.py
+++ receitas/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def receita(request):
+    return render(request, 'receita.html')
     # return HttpResponse('<h1> Receitas </h1> <br> <h2> Bem vindo </h2>')
