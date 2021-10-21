6efb15e50ad56a70f4cfe9b2e4409894c6a95ed6
Vinicius Dos Reis Oliveira <v_reis@outlook.com.br>
2021-10-21 10:02:03 -0300

    começando a utilizar bd postgres

--- receitas/views.py
+++ receitas/views.py
@@ -3,7 +3,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    receitas = {
+        1: 'Lasanha',
+        2: 'Sopa de legumes',
+        3: 'Sorvete',
+        4: 'Bolo de Cenoura',
+    }
+    dados = {
+        'nome_das_receitas': receitas
+    }
+    return render(request, 'index.html', dados)
 
 
 def receita(request):
